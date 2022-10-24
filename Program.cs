8397d0468595e87dc6127659fdc0737b5743328a
a-tibi <ahmd_tibi@yahoo.com>
2022-10-24 18:10:58 +0400

    add codes

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Staircase
+{
+	class Program
+	{
+		public static void staircase(int n)
+		{
+			for (int i = 1; i < n + 1; i++)
+			{
+				Console.WriteLine(new String('#', i).PadLeft(n));
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			Console.Write("Enter the number of satirs:");
+			int n = Convert.ToInt32(Console.ReadLine().Trim());
+			staircase(n);
+			Console.ReadKey();
+		}
+	}
+}
